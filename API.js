71b6710952848033c1bc560630930e2926dc02d5
FernandoRoyano <fernandoroyano.dev@gmail.com>
2022-12-13 20:54:46 +0100

    planetas y clima

---
+++ API.js
@@ -0,0 +1,27 @@
+fetch("https://swapi.dev/api/planets")
+  .then((response) => response.json())
+  .then((response) => {
+    response.results.forEach((planet) => {
+      let planetBox = document.createElement("div");
+      document.querySelector("body").appendChild(planetBox);
+      planetBox.innerHTML = `
+        <h1>${planet.name}</h1>
+        <p>Clima: ${planet.climate}</p>
+        <p>Población: ${planet.population}</p>
+        `;
+      let charList = document.createElement("ul");
+      planetBox.appendChild(charList);
+
+      planet.residents.forEach((char) => {
+        fetch(char)
+          .then((response) => response.json())
+          .then((character) => {
+            let charEle = document.createElement("li");
+            charEle.innerText = `${character?.name}`;
+            charList.appendChild(charEle);
+          })
+          .catch((err) => console.log("Soy un fallo", err));
+      });
+    });
+  })
+  .catch((err) => console.log("Soy un fallo que no debería salir", err));
